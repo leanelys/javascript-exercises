f99da19c23550f00fe0d4c49a89d85fe7bb56961
leanelys <leanelys.gonzalez@upr.edu>
2025-01-11 16:57:40 -0400

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+
+  // °C = (°F - 32) × 5/9
+  let result = (fahrenheit - 32) * (5 / 9);
+  result = Math.round(result * 10) / 10;
+  return result;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+  // °F = (9/5) °C + 32
+  let result = (celsius * (9 / 5)) + 32;
+  result = Math.round(result * 10) / 10;
+  return result;
+
 };
+
+// npm test tempConversion.spec.js
 
 // Do not edit below this line
 module.exports = {
