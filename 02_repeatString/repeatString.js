e3242a318b398da74c27c5fdaab8d949e7111d32
leanelys <leanelys.gonzalez@upr.edu>
2025-01-11 14:12:49 -0400

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word, repeats) {
 
+    if (repeats < 0) {
+        return "ERROR";
+    }
+
+    let result = "";
+
+    for (i = 0; i < repeats; i++) {
+        result += word;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
