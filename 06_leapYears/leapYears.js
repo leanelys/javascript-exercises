38d39a0fd9937fd68e6818b0f3bd52e52ec06fa5
leanelys <leanelys.gonzalez@upr.edu>
2025-01-11 16:43:51 -0400

    Complete 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    let divisbleBy4 = year % 4 == 0;
+    let divisibleBy100 = year % 100 == 0;
+    let divisibleBy400 = year % 400 == 0;
+
+    if (divisbleBy4 && (!divisibleBy100 || divisibleBy400)) {
+        return true;
+    }
+    else {
+        return false;
+    }
 
 };
+
+// npm test leapYears.spec.js
 
 // Do not edit below this line
 module.exports = leapYears;
