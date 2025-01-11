45e2edd55f655f0b1722deb632128101f29e2cd7
leanelys <leanelys.gonzalez@upr.edu>
2025-01-11 16:20:41 -0400

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...params) {
+
+    const result = [];
+
+    array.forEach(element => {
+        if (!params.includes(element)) {
+            result.push(element);
+        }
+    });
+
+    return result;
+
 };
+
+// npm test removeFromArray.spec.js
 
 // Do not edit below this line
 module.exports = removeFromArray;
