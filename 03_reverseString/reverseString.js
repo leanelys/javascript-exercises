2ce0566e5fb9f4fe3d42c5d42385d7fc2097bfa9
leanelys <leanelys.gonzalez@upr.edu>
2025-01-11 14:17:36 -0400

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    let result = "";
+
+    for (i = word.length - 1; i >= 0; i--) {
+        result += word[i];
+    }
+
+    return result;
 };
+
+// npm test reverseString.spec.js
 
 // Do not edit below this line
 module.exports = reverseString;
