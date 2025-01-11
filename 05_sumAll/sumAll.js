ee6aec3d6c85357edf48e1a893c6fbdd33719101
leanelys <leanelys.gonzalez@upr.edu>
2025-01-11 16:35:47 -0400

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+
+    // non negative numbers
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+    // non integer numbers
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
+
+    let sum = 0;
+
+    if (start < end) {
+        for (i = start; i <= end; i++) {
+            sum += i;
+        }
+    }
+    else {
+        for (i = end; i <= start; i++) {
+            sum += i;
+        }
+    }
+
+    return sum;
 
 };
+
+// npm test sumAll.spec.js
 
 // Do not edit below this line
 module.exports = sumAll;
